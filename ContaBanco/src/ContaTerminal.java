e2db510df840d658aca8e35123926a1e21fdfdfe
Felipe Elvas Barbosa <felipe.100062@aluno.faculdadedescomplica.com.br>
2023-11-22 22:08:20 -0300

    commit desafio projeto contaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,43 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numeroConta = 0;
+        String numeroAgencia = "";
+        String nomeCliente = "";
+        double saldo = 0;
+
+        boolean inputValido = false;
+
+        while (!inputValido) {
+            try {
+                System.out.println("Por favor, digite o número da conta: ");
+                numeroConta = input.nextInt();
+
+                System.out.println("Por favor, digite o número da Agência: ");
+                numeroAgencia = input.next();
+
+                System.out.println("Digite seu nome: ");
+                nomeCliente = input.next();
+
+                System.out.println("Informe o seu saldo: ");
+                saldo = input.nextDouble();
+
+                inputValido = true; // Se chegou até aqui sem lançar exceção, a entrada é válida
+
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, insira os dados corretamente.");
+                input.nextLine(); // Limpa o buffer do scanner
+            }
+        }
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " +
+                numeroAgencia + ", conta " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque.");
+
+        // Fechar o scanner no final
+        input.close();
     }
 }
---
+++ ContaBanco/src/utilitarios/Cliente.java
@@ -0,0 +1,60 @@
+package utilitarios;
+// package programa;
+
+// public class Cliente {
+// 	private static int numero;
+
+//         private String nomeCliente;
+//         private String agencia;
+//         private double saldo; 
+
+
+//     public static void main(String nomeCliente, String agencia, String numero, double saldo){
+//         this.numero = numero;
+//         this.nomeCliente = nomeCliente;
+//         this.agencia = agencia;
+//         this.saldo = saldo;
+        
+//     }}
+//     public String getNumero(){
+//         return numero;
+//     }
+    
+//     public String getNomeCliente(){
+//         return nomeCliente;
+//     }
+    
+//     public String getAgencia()  {
+//         return agencia;
+//     }
+//     public Double getSaldo(){
+//         return saldo;
+//     }
+//     public static void setNumero(int numero) {
+//         ContaTerminal.numero = numero;
+//     }
+//     public void setNomeCliente(String nomeCliente) {
+//         this.nomeCliente = nomeCliente;
+//     }
+//     public void setAgencia(String agencia) {
+//         this.agencia = agencia;
+//     }
+//     public void setSaldo(double saldo) {
+//         this.saldo = saldo;
+//     }
+//         public String toString(){
+//             return "\nNumero: " + this.getNumero() +
+//                    "\nNome: " + this.getNomeCliente() +
+//                    "\nAgencia: " + this.getAgencia() +
+//                    "\nSaldo: " + this.getSaldo();
+        
+  
+//         System.out.println("Olá " + conta.getNomeCliente() +
+//                 ", obrigado por criar uma conta em nosso banco. Sua agência é " +
+//                 conta.getAgencia() + ", conta " + conta.getNumeroConta() +
+//                 " e seu saldo " + conta.getSaldo() + " já está disponível para saque.");
+
+//         // Fechando o scanner
+//         scanner.close();
+    
+// 	}
